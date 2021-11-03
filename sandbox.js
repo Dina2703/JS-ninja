db8975beff9074c25f7560615f16418e2e4fe560
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-03 00:04:27 -0400

    string concatenation, getting a character, length.

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,20 @@
-alert('hello');
+// alert('hello');
+// console.log(1);
+console.log('hello, world');
+let email = 'example@mail.com';
+console.log(email);
+
+//string concatenation 
+let firstName = "Sam";
+let secondName = "Sanderson";
+let fullName = firstName + ' '+ secondName;
+console.log(fullName);
+
+// to get a single character from a particular position using a [] notation on a  string
+console.log(fullName[0]);
+console.log(secondName[2]);
+
+// string lenght
+console.log(fullName.length);
+
+//string methods
