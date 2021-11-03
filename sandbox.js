fdfd1e589c1004ccfd4a2b0c6fe8ccc50130545a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-03 00:32:22 -0400

    comman string methods 1

--- sandbox.js
+++ sandbox.js
@@ -17,6 +17,7 @@ console.log(secondName[2]);
 // string lenght
 console.log(fullName.length);
 
+// methods are just functions that belongs to specifice object or object type.
 // string methods (with dot notation and curly braces).
 // these methods below DO NOT alter(change) the original value that we used them on.
 console.log(fullName.toLocaleUpperCase());
@@ -26,3 +27,12 @@ console.log(result, fullName);
 //finding an index of a character
 let index = email.indexOf('@');
 console.log(index);
+
+let result2 = email.lastIndexOf('i');
+console.log(result2);
+
+let result3 = email.slice(0, 4);
+console.log(result3);
+
+let result4 = email.substr(2, 5);
+console.log(result4);
